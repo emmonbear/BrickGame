48519adc76719cf902e5dea2f341809666d3d670
emmonbea <moskaleviluak@icloud.com>
2024-04-30 23:26:46 +0500

    feat: add main header

---
+++ src/brick_game/tetris/tetris.h
@@ -0,0 +1,11 @@
+/**
+ * @file tetris.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief Main header
+ * @version 1.0
+ * @date 2024-04-30
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
