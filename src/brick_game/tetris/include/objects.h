0ec9e5ab27bebfb5f4ff9c277bfea6e16a0c8f2e
emmonbea <moskaleviluak@icloud.com>
2024-05-03 11:40:56 +0500

    feat[back]: add objects.h

---
+++ src/brick_game/tetris/include/objects.h
@@ -0,0 +1,17 @@
+/**
+ * @file objects.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief 
+ * @version 1.0
+ * @date 2024-05-02
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef MODULES_OBJECTS_H
+#define MODULES_OBJECTS_H
+
+
+
+#endif // MODULES_OBJECTS_H
