940814ecdfa34e2a872e69477f6326ca8214ff1d
emmonbea <moskaleviluak@icloud.com>
2024-05-03 11:39:09 +0500

    feat[back]: add common header

---
+++ src/brick_game/tetris/include/common.h
@@ -0,0 +1,20 @@
+/**
+ * @file types.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief commmon header
+ * @version 1.0
+ * @date 2024-04-30
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef MODULES_COMMON_H
+#define MODULES_COMMON_H
+
+#include "./common.h"
+#include "./fsm.h"
+#include "./init.h"
+#include "./objects.h"
+
+#endif // MODULES_COMMON_H
