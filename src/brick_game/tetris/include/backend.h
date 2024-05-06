d61973e347507b02f7bc0b1d0d852bde99b8ec1f
emmonbea <moskaleviluak@icloud.com>
2024-05-06 15:39:24 +0500

    feat(back): add defines

--- src/brick_game/tetris/include/backend.h
+++ src/brick_game/tetris/include/backend.h
@@ -14,54 +14,61 @@
 
 #include <stdbool.h>
 
-/**
- * @brief Enumeration of user actions
- * 
- */
+/// @brief number of lines on Field
+#define F_ROWS 20     
+
+/// @brief number of columns on Field
+#define F_COLS 10
+
+/// @brief Tetromino size
+#define T_SIZE 4 
+
+/// @brief Cell size
+#define C_SIZE 2 
+
+/// @brief Size of border
+#define BOARD_SIZE 2
+
+/// @brief Emergency program termination in case of memory allocation error
+#define MEM_ALLOC_ERROR fprintf(stderr, "Memory allocation error\n");\
+                        exit(1);
+
+
+
+/// @brief Enumeration of user actions
 typedef enum {
-    Start,
-    Pause,
-    Terminate,
-    Left,
-    Right,
-    Up,
-    Down,
-    Action
+  Start,
+  Pause,
+  Terminate,
+  Left,
+  Right,
+  Up,
+  Down,
+  Action
 } UserAction_t;
 
-/**
- * @brief 
- * 
- */
+/// @brief 
 typedef struct {
-    int **field;
-    int **next;
-    int score;
-    int high_score;
-    int level;
-    int speed;
-    int pause;
+  int **field;
+  int **next;
+  int score;
+  int high_score;
+  int level;
+  int speed;
+  int pause;
 } GameInfo_t;
 
-/**
- * @brief 
- * 
- */
+/// @brief 
 typedef struct {
-  GameInfo_t game_info;
+  GameInfo_t *game_info;
 } singleton;
 
-/**
- * @brief Get the instance object
- * 
- * @return singleton* 
- */
-singleton *get_instance() {
-  static singleton instance;
-  return &instance;
-}
 
+singleton *get_instance();
+void init_game();
 void userInput(UserAction_t action, bool hold);
 GameInfo_t updateCurrentState();
+void destroy_game ();
+void init_game();
 
 #endif // MODULES_BACKEND_H
