ef54e90167dc31d687aa77e2fd699de6d819e76d
emmonbea <moskaleviluak@icloud.com>
2024-05-06 23:56:00 +0500

    feat(back): add tetrominos

--- src/brick_game/tetris/include/backend.h
+++ src/brick_game/tetris/include/backend.h
@@ -1,12 +1,12 @@
 /**
  * @file backend.h
  * @author emmonbea (moskaleviluak@icloud.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2024-05-05
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #ifndef MODULES_BACKEND_H
@@ -15,25 +15,30 @@
 #include <stdbool.h>
 
 /// @brief number of lines on Field
-#define F_ROWS 20     
+#define F_ROWS 20
 
 /// @brief number of columns on Field
 #define F_COLS 10
 
 /// @brief Tetromino size
-#define T_SIZE 4 
+#define T_SIZE 4
 
 /// @brief Cell size
-#define C_SIZE 2 
+#define C_SIZE 2
 
 /// @brief Size of border
 #define BOARD_SIZE 2
 
 /// @brief Emergency program termination in case of memory allocation error
-#define MEM_ALLOC_ERROR fprintf(stderr, "Memory allocation error\n");\
-                        exit(1);
+#define MEM_ALLOC_ERROR                         \
+  fprintf(stderr, "Memory allocation error\n"); \
+  exit(1);
 
+/// @brief Count of tetrominos
+#define NUM_FIGURES 7
 
+/// @brief Number of possible positions for tetrominos
+#define NUM_POSITIONS 5
 
 /// @brief Enumeration of user actions
 typedef enum {
@@ -47,7 +52,7 @@ typedef enum {
   Action
 } UserAction_t;
 
-/// @brief 
+/// @brief
 typedef struct {
   int **field;
   int **next;
@@ -58,17 +63,43 @@ typedef struct {
   int pause;
 } GameInfo_t;
 
-/// @brief 
+typedef struct {
+  int y;
+  int x;
+} location_t;
+
+typedef struct {
+  int type;
+  int orientation;
+  location_t loc;
+} tetromino_t;
+
+
+/// @brief
 typedef struct {
   GameInfo_t *game_info;
+  tetromino_t tet;
 } singleton;
 
 
+
+typedef enum {
+  TET_I,
+  TET_Z,
+  TET_S,
+  TET_T,
+  TET_L,
+  TET_J,
+  TET_O,
+} tet_type;
+
+int get_random_figure();
+void generate_new_figure();
 singleton *get_instance();
 void init_game();
 void userInput(UserAction_t action, bool hold);
 GameInfo_t updateCurrentState();
-void destroy_game ();
+void destroy_game();
 void init_game();
 
-#endif // MODULES_BACKEND_H
+#endif  // MODULES_BACKEND_H
--- src/brick_game/tetris/modules/backend.c
+++ src/brick_game/tetris/modules/backend.c
@@ -1,24 +1,24 @@
 /**
  * @file backend.c
  * @author emmonbea (moskaleviluak@icloud.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2024-05-05
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #include "../include/backend.h"
 
-#include <stdlib.h>
-#include <stdio.h>
 #include <locale.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * @brief Get the instance object
- * 
- * @return singleton* 
+ *
+ * @return singleton*
  */
 singleton *get_instance() {
   static singleton instance;
@@ -27,6 +27,7 @@ singleton *get_instance() {
 
 void init_game() {
   setlocale(LC_ALL, "");
+  srand(time(NULL));
   singleton *s = get_instance();
   s->game_info = (GameInfo_t *)malloc(sizeof(GameInfo_t));
 
@@ -43,7 +44,7 @@ void init_game() {
 
   for (int i = 0; i < F_ROWS; i++) {
     s->game_info->field[i] = (int *)calloc((F_COLS * C_SIZE), sizeof(int));
-    if(!s->game_info->field[i]) {
+    if (!s->game_info->field[i]) {
       destroy_game(s);
       MEM_ALLOC_ERROR
     }
@@ -66,12 +67,12 @@ void init_game() {
 
 /**
  * @brief Clear allocated memory
- * 
- * @param[out] s 
+ *
+ * @param[out] s
  */
-void destroy_game () {
+void destroy_game() {
   singleton *s = get_instance();
-  
+
   if (s->game_info) {
     if (s->game_info->field) {
       for (int i = 0; i < F_ROWS; i++) {
@@ -97,4 +98,60 @@ void destroy_game () {
     free(s->game_info);
     s->game_info = NULL;
   }
+}
+
+void generate_new_figure() {
+  // singleton *s = get_instance();
+  static const location_t tet[NUM_FIGURES][NUM_POSITIONS][T_SIZE] =
+      {
+          // I
+          {{{1, 0}, {1, 1}, {1, 2}, {1, 3}},
+           {{2, 0}, {2, 1}, {2, 2}, {2, 3}},
+           {{0, 1}, {1, 1}, {2, 1}, {3, 1}},
+           {{0, 2}, {1, 2}, {2, 2}, {3, 2}},
+           {{0, 3}, {1, 3}, {2, 3}, {3, 3}}},
+          // Z
+          {{{0, 0}, {0, 1}, {1, 1}, {1, 2}},
+           {{1, 1}, {1, 2}, {2, 2}, {2, 3}},
+           {{0, 1}, {1, 0}, {1, 1}, {2, 0}},
+           {{0, 2}, {1, 1}, {1, 2}, {2, 1}},
+           {{0, 3}, {1, 2}, {1, 3}, {2, 2}}},
+          // S
+          {{{0, 1}, {0, 2}, {1, 0}, {1, 1}},
+           {{0, 2}, {0, 3}, {1, 1}, {1, 2}},
+           {{0, 0}, {1, 0}, {1, 1}, {2, 1}},
+           {{0, 1}, {1, 1}, {1, 2}, {2, 2}},
+           {{0, 2}, {1, 2}, {1, 3}, {2, 3}}},
+          // T
+          {{{0, 0}, {0, 1}, {0, 2}, {1, 1}},
+           {{0, 1}, {0, 2}, {0, 3}, {1, 2}},
+           {{0, 2}, {1, 2}, {1, 3}, {2, 2}},
+           {{0, 2}, {1, 1}, {1, 2}, {2, 2}},
+           {{0, 2}, {1, 1}, {1, 2}, {1, 3}}},
+          // L
+          {{{0, 2}, {1, 2}, {2, 2}, {2, 3}},
+           {{0, 1}, {0, 2}, {0, 3}, {1, 1}},
+           {{0, 1}, {0, 2}, {1, 2}, {2, 2}},
+           {{0, 1}, {1, 1}, {2, 1}, {2, 2}},
+           {{0, 3}, {1, 1}, {1, 2}, {1, 3}}},
+          // J
+          {{{0, 2}, {1, 2}, {2, 2}, {2, 1}},
+           {{0, 1}, {1, 1}, {1, 2}, {1, 3}},
+           {{0, 1}, {0, 2}, {1, 1}, {2, 1}},
+           {{0, 1}, {0, 2}, {0, 3}, {1, 3}},
+           {{0, 3}, {1, 3}, {2, 3}, {2, 2}}},
+          // O
+          {{{0, 2}, {0, 3}, {1, 2}, {1, 3}},
+           {{0, 1}, {0, 2}, {1, 1}, {1, 2}},
+           {{0, 0}, {0, 1}, {1, 0}, {1, 1}},
+           {{1, 1}, {1, 2}, {2, 1}, {2, 2}},
+           {{1, 2}, {1, 3}, {2, 2}, {2, 3}}},
+      };
+  
+  int index = get_random_figure();
+  printf("index = %d\n", index);
+}
+
+int get_random_figure() {
+  return rand() % NUM_FIGURES;
 }
