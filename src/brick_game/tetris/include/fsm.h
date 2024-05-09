b1801d9668ed30e4e8b9ca00383a98aac15adf89
emmonbea <moskaleviluak@icloud.com>
2024-05-09 18:02:08 +0500

    feat(back): add fsm stages enumeration

---
+++ src/brick_game/tetris/include/fsm.h
@@ -0,0 +1,35 @@
+/**
+ * @file fsm.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-05-09
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef MODULES_FSM_H
+#define MODULES_FSM_H
+
+typedef enum {
+  START,  ///< the state in which the game waits for the player to press the
+          ///< ready to play button
+  SPAWN,  ///< the state the game switches to when creating the next block and
+          ///< selecting the next block to be spawned
+  SHIFTING,   ///< the state to which the game switches after the timer expires.
+              ///< In this state the current block moves down one level
+  MOVING,     ///< basic game state with user input processing - rotating
+              ///< blocks/moving blocks horizontally
+  PAUSE,      ///< the state in which the game is paused
+  ATTACHING,  ///< the state the game goes to after the current block "touches"
+              ///< the already fallen blocks or the ground. If a filled line is
+              ///< formed, it is destroyed and the remaining blocks are shifted
+              ///< down. If a block is stopped in the topmost row, the game goes
+              ///< to the state "game over"
+  GAME_OVER,  ///< the state that characterizes the end of the game
+} stage;
+
+
+
+#endif  // MODULES_FSM_H
