fc2216cac11a07688ea71f8a6f29219cb76a3a81
emmonbea <moskaleviluak@icloud.com>
2024-05-11 23:21:52 +0500

    feat(front): add init module

---
+++ src/gui/cli/include/init_gui.h
@@ -0,0 +1,19 @@
+/**
+ * @file init_gui.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-05-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef GUI_INIT_H
+#define GUI_INIT_H
+
+#include "./common_gui.h"
+
+void init_screen();
+
+#endif  // GUI_INIT_H
---
+++ src/gui/cli/modules/init_gui.c
@@ -0,0 +1,39 @@
+/**
+ * @file init_gui.c
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-05-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "../include/init_gui.h"
+
+#define COLOR_PINK 8
+
+void init_screen() {
+  initscr();
+  // cbreak();
+  noecho();
+  keypad(stdscr, TRUE);
+  // timeout(0);
+  curs_set(0);
+  // use
+  // if (!has_colors()) {
+  //   printw("Your terminal does not support colors.");
+  //   getch();
+  // }
+  start_color();
+  init_pair(0, COLOR_WHITE, COLOR_BLACK);
+  init_pair(1, COLOR_BLUE, COLOR_BLACK);
+  init_pair(2, COLOR_CYAN, COLOR_BLACK);
+  init_pair(3, COLOR_GREEN, COLOR_BLACK);
+  init_pair(4, COLOR_RED, COLOR_BLACK);
+  init_pair(5, COLOR_YELLOW, COLOR_BLACK);
+  init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
+  init_color(COLOR_PINK, 1000, 500, 1000);
+  init_pair(7, COLOR_PINK, COLOR_BLACK);
+}
+
