2e42346ee215aeabfa9d2ac9cfe09058ad4cdb4a
emmonbea <moskaleviluak@icloud.com>
2024-05-11 15:24:04 +0500

    feat(back): add figures module header

---
+++ src/brick_game/tetris/include/figures.h
@@ -0,0 +1,28 @@
+/**
+ * @file figure_generator.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-05-09
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef MODULES_FIGURE_GENERATOR
+#define MODULES_FIGURE_GENERATOR
+
+#include "../include/common.h"
+
+/// @brief Amount of tetrominoes
+#define NUM_TETROMINOS 7
+
+/// @brief Number of positions
+#define NUM_POSITIONS 5
+
+/// @brief The size of the tetromino
+#define TETROMINO_SIZE 4
+
+void generate_new_figure(singleton *s);
+
+#endif  // MODULES_FIGURE_GENERATOR
