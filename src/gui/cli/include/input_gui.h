b0bfe8404f640514fac918d0beecbbd878be0c11
emmonbea <moskaleviluak@icloud.com>
2024-05-11 23:22:21 +0500

    feat: add input module

---
+++ src/gui/cli/include/input_gui.h
@@ -0,0 +1,19 @@
+/**
+ * @file input_gui.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-05-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef GUI_INPUT_H
+#define GUI_INPUT_H
+
+#include "./common_gui.h"
+
+void get_user_input(UserAction_t *action);
+
+#endif  // GUI_INPUT_H
---
+++ src/gui/cli/modules/input_gui.c
@@ -0,0 +1,43 @@
+/**
+ * @file input_gui.c
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-05-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "../include/input_gui.h"
+
+void get_user_input(UserAction_t *action) {
+  switch (getch()) {
+    case KEY_LEFT:
+      *action = Left;
+      break;
+    case KEY_RIGHT:
+      *action = Right;
+      break;
+    case KEY_DOWN:
+      *action = Down;
+      break;
+    case ' ':
+      *action = Action;
+      break;
+    case '\n':
+      *action = Start;
+      break;
+    case 'p':
+    case 'P':
+      *action = Pause;
+      break;
+    case 'q':
+    case 'Q':
+      *action = Terminate;
+      break;
+    default:
+      printw("NONE");
+      break;
+  }
+}
