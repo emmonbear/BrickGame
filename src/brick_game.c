948b65159d66a9352038fb6233234e41a88c1704
emmonbea <moskaleviluak@icloud.com>
2024-05-13 14:21:26 +0500

    feat: new game_loop

--- src/brick_game.c
+++ src/brick_game.c
@@ -9,25 +9,60 @@
  * 
  */
 
-// #include "./gui/cli/include/frontend.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 #include "./brick_game/tetris/include/fsm.h"
+#include "./gui/cli/include/field_gui.h"
+#include "./gui/cli/include/input_gui.h"
+#include "./gui/cli/include/init_gui.h"
+
 
 static singleton *get_instance() {
   static singleton instance;
   return &instance;
 }
 
+static void game_loop() {
+  singleton *s = get_instance();
+  WINDOW *field, *next, *score, *level, *high_score;
+
+  field = newwin(FIELD_HEIGHT, FIELD_WIDTH, Y_CENTER_FIELD, X_CENTER_FIELD);
+  next = newwin(NEXT_HEIGHT, NEXT_WIDTH, Y_CENTER_NEXT, X_CENTER_NEXT);
+  score = newwin(SCORE_HEIGHT, SCORE_WIDTH, Y_CENTER_SCORE, X_CENTER_SCORE);
+  level = newwin(LEVEL_HEIGHT, LEVEL_WIDTH, Y_CENTER_LEVEL, X_CENTER_LEVEL);
+  high_score = newwin(HIGH_SCORE_HEIGHT, HIGH_SCORE_WIDTH, Y_CENTER_HIGH_SCORE, X_CENTER_HIGH_SCORE);
+  s->stage = START;
+  *(s->action) = -1;
+
+  generate_new_figure(s);
+
+  while(1) {
+    refresh();
+    run_state(s);
+    get_user_input(s->action); 
+
+    draw_field(s->game_info->field, field);
+    draw_next(s->game_info->next, next);
+    draw_score(s->game_info->score, score);
+    draw_level(s->game_info->level, level);
+    draw_high_score(s->game_info->high_score, high_score);
+  }
+}
+
+
+
+
 int main() {
   singleton *s = get_instance();
-  start_stage(s);
 
-  while(s->stage != GAME_OVER) {
-    run_state(s);
-  }
-  game_over_stage(s);
+  init_game(s);
+  init_screen();
+  game_loop();
+  endwin();
+  destroy_game(s);
+
   return 0;
 }
