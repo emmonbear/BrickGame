0be527cc5a183940e4939bdac65f6b332e52e6be
emmonbea <moskaleviluak@icloud.com>
2024-05-14 01:37:16 +0500

    feat(back): add rotate and additional checks

--- src/brick_game/tetris/modules/operations.c
+++ src/brick_game/tetris/modules/operations.c
@@ -13,6 +13,8 @@
 
 #include <stdlib.h>
 
+static void reset_position(singleton *s);
+
 void put_figure(singleton *s) {
   for (size_t i = 0; i < TETROMINO_SIZE; i++) {
     for (size_t j = 0; j < TETROMINO_SIZE; j++) {
@@ -49,9 +51,11 @@ void move_left(singleton *s) {
 }
 
 void move_right(singleton *s) {
-  remove_figure(s);
-  s->figure.x++;
-  put_figure(s);
+  if (can_move_right(s)) {
+    remove_figure(s);
+    s->figure.x++;
+    put_figure(s);
+  }
 }
 
 bool can_move_down(singleton *s) {
@@ -81,7 +85,9 @@ bool can_move_left(singleton *s) {
       if (s->figure.current_figure[i][j]) {
         if (s->figure.x + j < 1) {
           res = false;
-        } else if (s->game_info->field[s->figure.y + i][s->figure.x + j - 1]) {
+        } else if ((s->game_info
+                        ->field[s->figure.y + i][s->figure.x + j - 1]) &&
+                   !is_inside_figure(s, s->figure.y + i, s->figure.x + j - 1)) {
           res = false;
         }
       }
@@ -90,16 +96,81 @@ bool can_move_left(singleton *s) {
   return res;
 }
 
+bool can_move_right(singleton *s) {
+  bool res = true;
+
+  for (size_t i = 0; i < TETROMINO_SIZE; i++) {
+    for (size_t j = TETROMINO_SIZE; j > 0; j--) {
+      if (s->figure.current_figure[i][j]) {
+        if (s->figure.x + j > WIDTH - 2) {
+          res = false;
+        } else if (s->game_info->field[s->figure.y + i][s->figure.x + j + 1] &&
+                   !is_inside_figure(s, s->figure.y + i, s->figure.x + j + 1)) {
+          res = false;
+        }
+      }
+    }
+  }
+
+  return res;
+}
 bool is_inside_figure(singleton *s, int y, int x) {
   bool res = false;
   for (size_t i = 0; i < TETROMINO_SIZE; i++) {
     for (size_t j = 0; j < TETROMINO_SIZE; j++) {
-      if ((s->figure.current_figure[i][j]) && (s->figure.y + i) == y &&
-          (s->figure.x + j) == x) {
+      if ((s->figure.current_figure[i][j]) && ((s->figure.y + i) == y) &&
+          ((s->figure.x + j) == x)) {
         res = true;
       }
     }
   }
 
   return res;
+}
+
+void rotate_figure(singleton *s) {
+  int tmp[TETROMINO_SIZE][TETROMINO_SIZE];
+
+  // Transpose matrix
+  for (size_t i = 0; i < TETROMINO_SIZE; i++) {
+    for (size_t j = 0; j < TETROMINO_SIZE; j++) {
+      tmp[i][j] = s->figure.current_figure[j][i];
+    }
+  }
+
+  // Reversing columns
+  for (size_t i = 0; i < TETROMINO_SIZE; i++) {
+    for (size_t j = 0; j < TETROMINO_SIZE; j++) {
+      s->figure.current_figure[i][j] = tmp[i][TETROMINO_SIZE - 1 - j];
+    }
+  }
+  reset_position(s);
+}
+
+static void reset_position(singleton *s) {
+  int min_x = TETROMINO_SIZE / 2;
+  int min_y = TETROMINO_SIZE / 2;
+
+  for (size_t i = 0; i < TETROMINO_SIZE; i++) {
+    for (size_t j = 0; j < TETROMINO_SIZE; j++) {
+      if (s->figure.current_figure[i][j]) {
+        if (i < min_y) {
+          min_y = i;
+        }
+        if (j < min_x) {
+          min_x = j;
+        }
+      }
+    }
+  }
+
+  for (size_t i = 0; i < TETROMINO_SIZE; i++) {
+    for (size_t j = 0; j < TETROMINO_SIZE; j++) {
+      if (s->figure.current_figure[i][j]) {
+        s->figure.current_figure[i - min_y][j - min_x] =
+            s->figure.current_figure[i][j];
+        s->figure.current_figure[i][j] = 0;
+      }
+    }
+  }
 }
