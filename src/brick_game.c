1ecc24a944c2d791219acb1c3f8b39b57a0d9156
emmonbea <moskaleviluak@icloud.com>
2024-08-07 20:15:28 +0500

    fix(tetris): interface decomposition

--- src/brick_game.c
+++ src/brick_game.c
@@ -14,58 +14,58 @@
 #include "./gui/cli/include/init_gui.h"
 #include "./gui/cli/include/input_gui.h"
 
+static Game_t game;
+
+static void draw_game(windows *wins) {
+  refresh();
+  switch (game.stage) {
+    case START:
+      draw_start_screen(wins->start.w);
+      break;
+    case PAUSE:
+      draw_pause();
+      break;
+    case GAME_OVER:
+      draw_game_over(wins->game_over.w, game.game_info.score,
+                     game.game_info.high_score);
+      break;
+    default:
+      draw_field(game.game_info.field, wins->field.w);
+      draw_next(game.game_info.next, wins->next.w);
+      draw_score(game.game_info.score, wins->score.w);
+      draw_level(game.game_info.level, wins->level.w);
+      draw_high_score(game.game_info.high_score, wins->high_score.w);
+      draw_info(wins->info.w);
+      break;
+  }
+}
+
 static void game_loop() {
-  Game_t *s = get_instance();
   windows *wins = init_windows();
-
-  s->stage = START;
-  (s->action) = -1;
-
-  reset_game_info(s);
-  generate_new_figure(s);
-
+  game.stage = START;
+  game.action = -1;
   int lines = LINES;
   int cols = COLS;
 
-  while (!s->game_over) {
-    update_windows(&wins, &lines, &cols);
-    refresh();
-    run_state(s);
-    get_user_input(&(s->action));
+  reset_game_info(&game);
+  generate_new_figure(&game);
 
-    switch (s->stage) {
-      case START:
-        draw_start_screen(wins->start.w);
-        break;
-      case PAUSE:
-        draw_pause();
-        break;
-      case GAME_OVER:
-        draw_game_over(wins->game_over.w, s->game_info.score,
-                       s->game_info.high_score);
-        break;
-      default:
-        draw_field(s->game_info.field, wins->field.w);
-        draw_next(s->game_info.next, wins->next.w);
-        draw_score(s->game_info.score, wins->score.w);
-        draw_level(s->game_info.level, wins->level.w);
-        draw_high_score(s->game_info.high_score, wins->high_score.w);
-        draw_info(wins->info.w);
-        break;
-    }
+  while (!game.game_over) {
+    update_windows(&wins, &lines, &cols);
+    run_state(&game);
+    get_user_input(&game.action);
+    draw_game(wins);
   }
+
   destroy_windows(&wins);
 }
 
 int main() {
-  Game_t *s = get_instance();
-
-  init_game(s);
+  init_game(&game);
   init_screen();
   game_loop();
   endwin();
-
-  destroy_game(s);
+  destroy_game(&game);
 
   return 0;
 }
