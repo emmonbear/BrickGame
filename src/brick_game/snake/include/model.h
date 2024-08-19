8f79fbe980bd64b9ebf1aeba1dc5595c8abc9f32
emmonbea <moskaleviluak@icloud.com>
2024-08-19 22:12:44 +0500

    feat(snake): update Model

--- src/brick_game/snake/include/model.h
+++ src/brick_game/snake/include/model.h
@@ -1,12 +1,12 @@
 /**
  * @file model.h
  * @author emmonbea (moskaleviluak@icloud.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2024-08-16
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #ifndef SRC_BRICK_GAME_SNAKE_INCLUDE_MODEL_H_
@@ -16,14 +16,30 @@ extern "C" {
 #include "../../../common/include/common.h"
 }
 
+#include <vector>
+
 namespace s21 {
-  class SnakeModel {
-   public:
-    SnakeModel();
-    ~SnakeModel();
-    GameInfo_t info_;
-   private:
-  };
-}
+class SnakeModel {
+  friend class SnakeModelTest;
+ public:
+  SnakeModel();
+  ~SnakeModel();
+
+  void InitializeGame();
+
+ private:
+  GameInfo_t info_;
+  std::vector<std::pair<int, int>> snake_;
+  std::pair<int, int> food_;
+
+  void GenerateFood();
+  void UpdateField();
+  void ClearField();
+  void PlaceSnakeOnField();
+  void PlaceFoodOnField();
+
+
+};
+}  // namespace s21
 
 #endif  // SRC_BRICK_GAME_SNAKE_INCLUDE_MODEL_H_
--- src/brick_game/snake/modules/model.cc
+++ src/brick_game/snake/modules/model.cc
@@ -1,28 +1,79 @@
 /**
  * @file model.cpp
  * @author emmonbea (moskaleviluak@icloud.com)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2024-08-16
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #include "../include/model.h"
 
-namespace s21 {
-  SnakeModel::SnakeModel() {
-    allocate_2d_array(&info_.field, HEIGHT, WIDTH);
-    info_.next = nullptr;
-    info_.high_score = load_max_score();
-    info_.level = 1;
-    info_.speed = 1;
-    info_.score = 0;
-    info_.pause = 0;
-  }
+#include <cstdlib>  // for crand
+#include <ctime>    // for time
 
-  SnakeModel::~SnakeModel() {
-    destroy_2d_array(&info_.field, HEIGHT);
+namespace s21 {
+SnakeModel::SnakeModel() {
+  std::srand(static_cast<unsigned>(std::time(nullptr)));
+  InitializeGame();
+}
+
+SnakeModel::~SnakeModel() { destroy_2d_array(&info_.field, HEIGHT); }
+
+void SnakeModel::InitializeGame() {
+  allocate_2d_array(&info_.field, HEIGHT, WIDTH);
+  info_.next = nullptr;
+  info_.high_score = load_max_score();
+  info_.level = 1;
+  info_.speed = 1;
+  info_.score = 0;
+  info_.pause = 0;
+  snake_ = {{HEIGHT / 2, WIDTH / 2},
+            {HEIGHT / 2, WIDTH / 2 - 1},
+            {HEIGHT / 2, WIDTH / 2 - 2}};
+  GenerateFood();
+}
+
+void SnakeModel::GenerateFood() {
+  bool valid_position = false;
+
+  while (!valid_position) {
+    food_ = {std::rand() % HEIGHT, std::rand() % WIDTH};
+    valid_position = true;
+
+    for (const auto &iter : snake_) {
+      if (iter == food_) {
+        valid_position = false;
+        break;
+      }
+    }
   }
 }
+
+void SnakeModel::UpdateField() {
+  ClearField();
+  PlaceSnakeOnField();
+  PlaceFoodOnField();
+}
+
+void SnakeModel::ClearField() {
+  for (int i = 0; i < HEIGHT; ++i) {
+    for (int j = 0; j < WIDTH; ++j) {
+      info_.field[i][j] = 0;
+    }
+  }
+}
+
+void SnakeModel::PlaceSnakeOnField() {
+  for (const auto &segment : snake_) {
+    info_.field[segment.first][segment.second] = 1;
+  }
+}
+
+void SnakeModel::PlaceFoodOnField() {
+  info_.field[food_.first][food_.second] = 2;
+}
+
+}  // namespace s21
