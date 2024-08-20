5de8a3833ac9055ed7b2f241a6b218f92c413654
emmonbea <moskaleviluak@icloud.com>
2024-08-20 21:55:22 +0500

    feat(snake): add controller

---
+++ src/controller/snake/modules/controller.cc
@@ -0,0 +1,17 @@
+/**
+ * @file controller.cc
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief 
+ * @version 1.0
+ * @date 2024-08-20
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "../include/controller.h"
+
+namespace s21 {
+  SnakeController::SnakeController(SnakeModel *model) : model_(model) {}
+  
+}
