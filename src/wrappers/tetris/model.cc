83fff3143e4317afb39437b0a16b498e3e17a105
emmonbea <moskaleviluak@icloud.com>
2024-08-21 20:22:55 +0500

    feat(tetris): add wrapper for tetris model

---
+++ src/wrappers/tetris/model.cc
@@ -0,0 +1,12 @@
+/**
+ * @file model.cc
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-08-21
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "./model.h"
---
+++ src/wrappers/tetris/model.h
@@ -0,0 +1,15 @@
+/**
+ * @file model.cc
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-08-21
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_WRAPPERS_TETRIS_MODEL_H_
+#define SRC_WRAPPERS_TETRIS_MODEL_H_
+
+#endif  // SRC_WRAPPERS_TETRIS_MODEL_H_
