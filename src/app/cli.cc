0b277ff277fcbf980001c5cfdd477942259b59d3
emmonbea <moskaleviluak@icloud.com>
2024-09-10 09:12:04 +0500

    fix: update main cli

---
+++ src/app/cli.cc
@@ -0,0 +1,35 @@
+/**
+ * @file cli.cc
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief 
+ * @version 1.0
+ * @date 2024-09-08
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "../include/wrappers/tetris_model.h"
+#include "../include/wrappers/cli_view.h"
+#include "../include/controller/controller.h"
+
+#include <iostream>
+
+int main() {
+  std::cout << "Выберите игру: 1 - тетрис, 2 - змейка" << std::endl;
+  int choise = 0;
+  std::cin >> choise;
+  s21::IModel *model = nullptr;
+  
+
+  if (choise == 1) {
+    model = new s21::TetrisModel();
+    s21::Controller controller(model);
+    s21::CliView view(controller);
+    view.startEventLoop();
+  } else if (choise == 2) {
+    std::cout << "Змейка" << std::endl;
+  }
+
+  return 0;
+}
