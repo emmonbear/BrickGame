3d7d65c7bb56fd01c5adcc49a59f33d194255390
emmonbea <moskaleviluak@icloud.com>
2024-09-13 12:26:41 +0500

    feat(desktop): add view class

---
+++ src/gui/desktop/desktop_view.cc
@@ -0,0 +1,12 @@
+/**
+ * @file desktop_view.cc
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-09-13
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "gui/desktop/desktop_view.h"
---
+++ src/include/gui/desktop/desktop_view.h
@@ -0,0 +1,15 @@
+/**
+ * @file desktop_view.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief
+ * @version 1.0
+ * @date 2024-09-13
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_INCLUDE_GUI_DESKTOP_DESKTOP_VIEW_H_
+#define SRC_INCLUDE_GUI_DESKTOP_DESKTOP_VIEW_H_
+
+#endif  // SRC_INCLUDE_GUI_DESKTOP_DESKTOP_VIEW_H_
